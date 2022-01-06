cbda2e910a7b40b231437cc8881b690744b3572a
developeremdad <developeremdad@gmail.com>
2022-01-06 23:01:37 +0600

    register form design

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -7,13 +7,16 @@ import "./Register.css";
 const Registration = () => {
     return (
         <div className="registration">
-             <Header></Header>
-                <Row xs={1} md={1} lg={2} style={{ width: "100%" }}>
+             <Header />
+                <div className="container my-5">
+                <Row xs={1} md={1} lg={2}>
                     <Col  >
-                        <h1>Registration here</h1>
+                        {/* <h1>Registration here</h1> */}
             
-                        <Container className="form" >
+                        <Container className="form py-3" >
                            <Form >
+                           <h2 className='pt-2'>REGISTER FORM</h2>
+                           <hr className='w-50 mx-auto border border-2 border-dark' />
                                 <Form.Group className="mb-3" controlId="formGroupName">
                                     <Form.Label className="form-label">Your Full Name</Form.Label>
                                     <Form.Control name='name' type="text" placeholder="full name" className="form-input" />
@@ -43,16 +46,17 @@ const Registration = () => {
                                 </Button>
                                 <br /> <br />
                                 <h4>already registerd?Back to</h4>
-                                <Link className="login-text" to="/login"> <h1 >login</h1> </Link>
+                                <Link className="login-text" to="/login"> <h5>LOGIN</h5> </Link>
                             </Form>
                         </Container> 
 
 
                     </Col>
-                    <Col style={{ marginBottom: '5px' }}>
-                        <Image style={{ height: '900px' }} fluid src="https://i.ibb.co/LkLzp6m/New-entries-pana-1.png" alt="" />
+                    <Col>
+                        <Image fluid src="https://i.ibb.co/LkLzp6m/New-entries-pana-1.png" alt="" />
                     </Col>
                 </Row>
+                </div>
         </div>
     );
 };
